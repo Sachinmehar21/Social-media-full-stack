6fd588c0dc9885c4618e73730dc3e54d90a080c8
Sachin <130161660+Sachinmehar21@users.noreply.github.com>
2025-07-08 13:12:57 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { Register, Login, logout, forgotPassword } = require("../controllers/authControllers");
+const {
+  Register,
+  Login,
+  logout,
+  forgotPassword,
+  googleAuth
+} = require("../controllers/authControllers");
 
 const router = express.Router();
 
-router.post("/", Register); // Handle user registration
-router.post("/login", Login); // Handle login
-router.post("/logout", logout); // Logout user
+router.post("/", Register);                 // Register route
+router.post("/login", Login);              // Login route
+router.post("/logout", logout);            // Logout route
 router.get("/forgotpassword", forgotPassword); // Forgot password page
+router.post("/auth/google", googleAuth);   // Google OAuth route
 
 module.exports = router;
