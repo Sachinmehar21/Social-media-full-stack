c6ffae1c9eedfbd8f9ec113808174635f189c3cb
Sachin <130161660+Sachinmehar21@users.noreply.github.com>
2025-07-08 14:34:29 +0530

    socket

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 export const API_URL = import.meta.env.VITE_API_URL;
 
+// Axios instance
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
@@ -15,4 +17,9 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api; 
+// Socket.IO instance
+export const socket = io(API_URL, {
+  withCredentials: true,
+});
+
+export default api;
