195a0e3d5209c46116cb63345a005df14e4680a1
Sachin <130161660+Sachinmehar21@users.noreply.github.com>
2025-12-19 15:46:17 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,10 +19,7 @@ console.log("MONGO_URL from .env:", process.env.MONGO_URL);
 // CORS setup for dev + Vercel
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://social-media-full-stack-n79t.vercel.app",
-  "https://social-media-full-stack-k8mq.vercel.app",
-  "https://social-media-full-stack-six.vercel.app"
-  
+  "https://social-media-full-stack-six.vercel.app",
 ];
 
 app.use(cors({
